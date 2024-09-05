ea01ba83256a6e9d8a8117caca74cad3a0c40517
Valery Rabchenyuk <valery0623@gmail.com>
2024-09-05 16:32:26 +0300

    Bootstrap

--- acme_project/birthday/forms.py
+++ acme_project/birthday/forms.py
@@ -3,14 +3,28 @@
 from django import forms
 
 
-class BirthdayForm(forms.Form):
-    first_name = forms.CharField(label='Имя', max_length=20)  # полей, атрибут класса BirthdayForm
-    last_name = forms.CharField(        # текстовое поле с доп атрибутом
-        label='Фамилия',        # аргументе label для кастомного названия полей
-        required=False,         # делает поле необязательным (по умол всегда True)
-        help_text='Необязательное поле'     # подсказка
-        )
-    birthday = forms.DateField(       # поле с датой
-        label='Дата рождения',
-        widget=forms.DateInput(attrs={'type': 'date'})      # виджет для ввода даты должен с типом date.
-        )
+from .models import Birthday
+
+
+#Изменили с forms.Form
+class BirthdayForm(forms.ModelForm):
+
+       # Все настройки задаём в подклассе Meta.
+    class Meta:
+        # Указываем модель, на основе которой должна строиться форма.
+        model = Birthday
+        # Указываем, что надо отобразить все поля.
+        fields = '__all__'
+        widgets = {
+            'birthday': forms.DateInput(attrs={'type': 'date'})
+        }
+    # first_name = forms.CharField(label='Имя', max_length=20)  # полей, атрибут класса BirthdayForm
+    # last_name = forms.CharField(        # текстовое поле с доп атрибутом
+    #     label='Фамилия',        # аргументе label для кастомного названия полей
+    #     required=False,         # делает поле необязательным (по умол всегда True)
+    #     help_text='Необязательное поле'     # подсказка
+    #     )
+    # birthday = forms.DateField(       # поле с датой
+    #     label='Дата рождения',
+    #     widget=forms.DateInput(attrs={'type': 'date'})      # виджет для ввода даты должен с типом date.
+    #     )
--- acme_project/birthday/models.py
+++ acme_project/birthday/models.py
@@ -1,1 +1,9 @@
 from django.db import models
+
+
+class Birthday(models.Model):
+    first_name = models.CharField('Имя', max_length=20)    # либо verbose_name="Имя"
+    last_name = models.CharField(
+       'Фамилия', blank=True, help_text='Необязательное поле', max_length=20   # blank=True вместо required=False (допустимы пустые значения);
+    )
+    birthday = models.DateField('Дата рождения')
