7b9976ee15edab18e7ef37651407a8537c077a7d
Valery Rabchenyuk <valery0623@gmail.com>
2024-09-06 16:57:23 +0300

    validators

--- acme_project/birthday/forms.py
+++ acme_project/birthday/forms.py
@@ -1,15 +1,19 @@
 """файл, в котором хранят код, управляющий формами"""
 
 from django import forms
-
+# Импортируем класс ошибки валидации.
+from django.core.exceptions import ValidationError
 
 from .models import Birthday
+
+# Множество с именами участников Ливерпульской четвёрки.
+
+BEATLES = {'Джон Леннон', 'Пол Маккартни', 'Джордж Харрисон', 'Ринго Старр'} # см. фу-ю clean(self)
 
 
 #Изменили с forms.Form
 class BirthdayForm(forms.ModelForm):
-
-       # Все настройки задаём в подклассе Meta.
+    # Все настройки задаём в подклассе Meta.
     class Meta:
         # Указываем модель, на основе которой должна строиться форма.
         model = Birthday
@@ -18,6 +22,31 @@ class BirthdayForm(forms.ModelForm):
         widgets = {
             'birthday': forms.DateInput(attrs={'type': 'date'})
         }
+
+    def clean_first_name(self):
+        # Получаем значение имени из словаря очищенных данных.
+        first_name = self.cleaned_data['first_name']
+        # Разбиваем полученную строку по пробелам и возвращаем только первое имя.
+        return first_name.split()[0]
+
+    def clean(self):
+        # Получаем имя и фамилию из очищенных полей формы.
+        first_name = self.cleaned_data['first_name']
+        last_name = self.cleaned_data['last_name']
+        # Проверяем вхождение сочетания имени и фамилии во множество имён.
+        if f'{first_name} {last_name}' in BEATLES:
+            raise ValidationError(
+                'Мы тоже любим Битлз, но введите, пожалуйста, настоящее имя!'
+            ) 
+
+
+
+
+    # # Импортируем функцию-валидатор.
+    # from .validators import real_age
+    # 
+    # 
+    # 
     # first_name = forms.CharField(label='Имя', max_length=20)  # полей, атрибут класса BirthdayForm
     # last_name = forms.CharField(        # текстовое поле с доп атрибутом
     #     label='Фамилия',        # аргументе label для кастомного названия полей
@@ -26,5 +55,9 @@ class BirthdayForm(forms.ModelForm):
     #     )
     # birthday = forms.DateField(       # поле с датой
     #     label='Дата рождения',
-    #     widget=forms.DateInput(attrs={'type': 'date'})      # виджет для ввода даты должен с типом date.
+    #     widget=forms.DateInput(attrs={'type': 'date'}     # виджет для ввода даты должен с типом date.
+
+    # В аргументе validators указываем список или кортеж 
+    # валидаторов этого поля (валидаторов может быть несколько).
+    #    validators=(real_age,),
     #     )
--- acme_project/birthday/models.py
+++ acme_project/birthday/models.py
@@ -1,9 +1,12 @@
 from django.db import models
 
+# Импортируется функция-валидатор.
+from .validators import real_age
+
 
 class Birthday(models.Model):
     first_name = models.CharField('Имя', max_length=20)    # либо verbose_name="Имя"
     last_name = models.CharField(
        'Фамилия', blank=True, help_text='Необязательное поле', max_length=20   # blank=True вместо required=False (допустимы пустые значения);
     )
-    birthday = models.DateField('Дата рождения')
+    birthday = models.DateField('Дата рождения', validators=(real_age,)) # Валидатор указывается в описании поля.
